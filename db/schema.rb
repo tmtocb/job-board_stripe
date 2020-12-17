2eda8588b7beac75737b11e3b329993a24f271d1
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-17 12:42:23 +0100

    add Card info to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_185349) do
+ActiveRecord::Schema.define(version: 2020_12_17_113215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_12_16_185349) do
     t.string "name"
     t.string "username"
     t.boolean "admin"
+    t.string "stripe_id"
+    t.string "card_brand"
+    t.string "card_last4"
+    t.string "card_exp_month"
+    t.string "card_exp_year"
+    t.datetime "expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
